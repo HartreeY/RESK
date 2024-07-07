317a298abf637852ee6e36cb77aa3a08e29113c7
Vindenatt <roentg@gmail.com>
2024-07-07 13:01:39 +0900

    func descriptions done + formatted

--- programs/consts.jl
+++ programs/consts.jl
@@ -1,42 +1,48 @@
-# Constants
+# Default constants
 # ------------------------------------------------
 # Edit these to your preference.
 # ================================================
 
-# Space parameters
+# Space bounds
+# ------------------------------------------------
 DEF_X_MAX = 100
 DEF_Y_MAX = 10
 DEF_Z_MAX = 10
 
 # Population parameters
-DEF_N_DEMES_STARTFILL = 5
+# ------------------------------------------------
+DEF_N_DEMES_STARTFILL = 5 # Number of demes filled at start
 DEF_K_CAPACITY = 20
-DEF_R_PROLIF_RATE = 1.8
+DEF_R_PROLIF_RATE = 1.8 # Proliferation rate
 DEF_r_LOG_PROLIF_RATE = log(DEF_R_PROLIF_RATE)
 
-# Gene parameters (regular)
+# Gene parameters (finite-sites)
+# ------------------------------------------------
 DEF_N_LOCI = 1000
-DEF_N_SEL_LOCI = 500
-DEF_MUT_RATE = 0.7567 # genome-wide
-DEF_MIGR_RATE = 0.1
-DEF_S_SEL_COEF = 0.002
-DEF_H_DOMIN_COEF = 0
-DEF_PROP_OF_DEL_MUTS = 0.9
+DEF_N_SEL_LOCI = 500 # No. of selected loci in the finite-sites model
+DEF_MUT_RATE = 0.7567 # Genome-wide mutation rate
+DEF_MIGR_RATE = 0.1 # Migration rate
+DEF_S_SEL_COEF = 0.002 # Selection coefficient
+DEF_H_DOMIN_COEF = 0 # Dominance coefficient
+DEF_PROP_OF_DEL_MUTS = 0.9 # Proportion of deleterious mutations in nature
 
 # Gene parameters (infinite-sites)
-DEF_N_SEGR_REGIONS = 20
-DEF_PROP_OF_SEL_LOCI = 1.0
+# ------------------------------------------------
+DEF_N_SEGR_REGIONS = 20 # No. of segregating regions
+DEF_PROP_OF_SEL_LOCI = 1.0 # Proportion of selected loci within those regions
 
 # Expansion parameters
+# ------------------------------------------------
 DEF_X_MAX_BURNIN = 5
-DEF_R_MAX_BURNIN = 3
-DEF_N_GENS_BURNIN = 10
+DEF_R_MAX_BURNIN = 3 # Radius that bounds the burn-in area
+DEF_N_GENS_BURNIN = 10 # No. of burn-in generations
 DEF_X_MAX_EXP = DEF_X_MAX
 DEF_Y_MAX_EXP = DEF_Y_MAX
-DEF_R_MAX_EXP = 20
-DEF_N_GENS_EXP = 40
-DEF_MIGR_MODE = "ort"
+DEF_R_MAX_EXP = 20 # Radius that bounds the expansion area
+DEF_N_GENS_EXP = 40 # No. of expansion generations
+DEF_MIGR_MODE = "ort" # Migration mode
 DEF_DATA_TO_GENERATE = "FP"
 
 # Other
-DEF_AUTO_GRAPHS = false
+# ------------------------------------------------
+DEF_AUTO_GRAPHS = false # Scale graphs automatically?
