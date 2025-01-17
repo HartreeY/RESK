068adeb2b286ba33874b10ae4f3ff16e11db9d5e
Vindenatt <roentg@gmail.com>
2025-01-17 20:54:27 +0900

    reorganised branch files

--- study/download1.ps1
+++
@@ -1,7 +0,0 @@
-$sourcePath = "~/rangeexp/resk/data/test_fst.re"
-$destFolder = $PSScriptRoot + "\data\"
-$compName = "raiden.riken.jp"
-$cred = "hartree"
-$privateKeyPath = "C:\Users\Hartree\Documents\raiden\private"
-
-Get-SCPItem -ComputerName $compName -Credential $cred -KeyFile $privateKeyPath -PathType File -Destination $destFolder -Path $sourcePath
