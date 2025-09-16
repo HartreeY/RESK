815a61314963f8b914f8993920356bea1725521e
Hartree <roentg@gmail.com>
2025-09-16 22:21:01 +0900

    gitignore changed (2)

--- dev/fst/2d_octfst2.jl
+++
@@ -1,7 +0,0 @@
-@everywhere include("resk.jl")
-
-test, time = @timed rangeexp_strip(50,400,10; x_max_burnin=3, x_max_exp=130, y_max=5,
-        data_to_generate="FPCM",capacity=35, prolif_rate=1.8, n_loci=1000,
-        n_sel_loci=312, mut_rate=1,migr_rate=0.145,sel_coef=0.01,bottleneck=NaN,migr_mode="diag1/2")
-serialize("data/2d_octfst.re", test)
-println(time)
--- dev/scp_from_hpc_example.ps1
+++
@@ -1,7 +0,0 @@
-$sourcePath = "~/rangeexp/resk/data/ooa_move3.re"
-$destFolder = $PSScriptRoot + "\data\"
-$compName = "***.riken.jp"
-$cred = "***"
-$privateKeyPath = "C:\Users\***\Documents\***\private"
-
-Get-SCPItem -ComputerName $compName -Credential $cred -KeyFile $privateKeyPath -PathType File -Destination $destFolder -Path $sourcePath
